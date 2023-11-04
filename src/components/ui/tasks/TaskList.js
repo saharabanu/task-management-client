104df3a6f33c3a9c0355f96a8848043ef2a026e4
saharabanu <saharabanungn@gmail.com>
2023-11-04 16:42:48 +0600

    task delete done

--- src/components/ui/tasks/TaskList.js
+++ src/components/ui/tasks/TaskList.js
@@ -1,54 +1,74 @@
-'use client'
+"use client";
 
-import { useGetTasksQuery } from "@/redux/api/apiSlice"
-
-
+import { useDeleteTaskMutation, useGetTasksQuery } from "@/redux/api/apiSlice";
+import Swal from "sweetalert2";
 
 const TaskList = () => {
-
-  const {data} = useGetTasksQuery();
+  // get all task query
+  const { data } = useGetTasksQuery();
   const tasks = data?.data;
   // console.log(data)
-  
-  
+
+  // delete task
+
+  const [deleteTask] = useDeleteTaskMutation();
+
+  // delete function
+
+  const deleteFunc = (id) => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You will delete this task!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteTask(id)
+        Swal.fire(
+          'Deleted!',
+          'This task has been deleted.',
+          'success'
+        )
+      }
+    })
+  };
+
   return (
     <div className=" border max-w-3xl mx-auto">
-   <div>
-   <table className="table-auto">
-  <thead>
-    <tr>
-      <th>Task</th>
-      <th>Description</th>
-      <th>Priority</th>
-      <th>Status</th>
-      <th>Action</th>
-     
-    </tr>
-    
-  </thead>
-  <hr />
-  <tbody>
-  {tasks?.map((item)=>(<tr key={item?.id}>
-      <td>{item?.title} </td>
-      <td className="pl-5">{item?.description}</td>
-      <td className="pl-5">{item?.priority}</td>
-      <td className="pl-5">{item?.status}</td>
-      
-      <td className="pl-5">
-      <button className="">Edit</button>
-      <button className="px-3">delete</button>
-      </td>
-      
-     
-    </tr>
-    ))}
-  </tbody>
-</table>
-   </div>
-        
-    </div>
-    
-  )
-}
+      <div>
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Description</th>
+              <th>Priority</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <hr />
+          <tbody>
+            {tasks?.map((item) => (
+              <tr key={item?.id}>
+                <td>{item?.title} </td>
+                <td className="pl-5">{item?.description}</td>
+                <td className="pl-5">{item?.priority}</td>
+                <td className="pl-5">{item?.status}</td>
 
-export default TaskList
+                <td className="pl-5">
+                  <button className="">Edit</button>
+                  <button className="px-3" onClick={() =>deleteFunc(item?.id)}>delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -1,22 +1,36 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
-
+const TASKS = '/tasks';
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:5000/api/v1",
   }),
 
-  tagTypes: ["tasks"],
+  tagTypes: ["tasks", 'task'],
   endpoints: (builder) => ({
+    // get all tasks
     getTasks: builder.query({
       query: () => ({
-        url: "/tasks",
+        url: TASKS,
       }),
       providesTags: ["tasks"],
     }),
+
+    // delete task
+
+    deleteTask: builder.mutation({
+        query: (id) => ({
+          url: `${TASKS}/${id}`,
+          method: "DELETE",
+        }),
+        invalidatesTags: ["tasks"],
+      }),
+
+
+
   }),
 });
 
-export const { useGetTasksQuery } = apiSlice;
+export const { useGetTasksQuery, useDeleteTaskMutation } = apiSlice;
